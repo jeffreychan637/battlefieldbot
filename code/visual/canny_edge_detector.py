bb77dab8923864daf6aa6f1da4bfef713d8e73c8
William Tang <william-t@berkeley.edu>
2017-04-17 15:56:46 -0700

    Added Canny edge detection to wallet images

--- code/visual/canny_edge_detector.py
+++ code/visual/canny_edge_detector.py
@@ -1,9 +1,11 @@
-import cv2
+import sys
+sys.path.append('/usr/local/lib/python2.7/site-packages')
 import numpy as np
+import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('messi5.jpg',0)
-edges = cv2.Canny(img,100,200)
+img = cv2.imread('wallet3.JPG',0)
+edges = cv2.Canny(img,25,75)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
