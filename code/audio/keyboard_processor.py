74ca0a84da7e9a77441470fa1d62287dd6f09b4d
Jeffrey Chan <jeffreychan637@berkeley.edu>
2017-04-20 03:12:54 -0700

    Updated keyboard processor to queue commands and only send them one at a time to the GUI/PSOC

--- code/audio/keyboard_processor.py
+++ code/audio/keyboard_processor.py
@@ -1,18 +1,35 @@
+from multiprocessing import Queue
+from threading import Thread
+import time
+
+import audio_processor as Audio
+
+from socketIO_client import SocketIO, BaseNamespace
+
 COMMANDS = ["fire", "retrieve", "stop"]
 
-def send_command(command):
-    print("Command Sent: %s" % command)
-    #send command to PSOC
-    #send command to GUI
-
-def main():
+def keyboard_processor(commands):
     while True:
         command = raw_input("Enter Command: ")
         if command == "f":
-            send_command("fire")
+            commands.put("fire")
         elif command == "r":
-            send_command("retrieve")
+            commands.put("retrieve")
         elif command == "s":
-            send_command("stop")
+            commands.put("stop")
+
+def main():
+    socketIO = SocketIO('localhost', 5000)
+    audio_socket = socketIO.define(Audio.AudioNamespace, '/audio')
+    audio_socket.on('connect', audio_socket.on_connect)
+    audio_socket.on('disconnect', audio_socket.on_disconnect)
+
+    commands = Queue()
+
+    command_executor = Thread(target=Audio.execute_commands, args=(commands, audio_socket))
+    command_executor.daemon = True
+    command_executor.start()
+
+    keyboard_processor(commands)
 
 if __name__ == '__main__': main()
